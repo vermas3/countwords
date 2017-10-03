abcbd6d8c88df47d38b9385f99bc4c93d93719f0
vermas3 <vermas3@SOB-B2113-19>
2017-10-03 18:46:11 -0400

    countwords

--- Countwords/Countwords.cpp
+++ Countwords/Countwords.cpp
@@ -2,10 +2,39 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int countwords(char myphr[100]);
 
 
-int main()
-{
-    return 0;
+int main() {
+	char myphrase[100];
+	cout << "Enter phrase" << endl;
+	gets_s(myphrase);
+
+	int cw = countwords(myphrase);
+	cout << " you have " << cw << " words " << endl;
+	return 0;
 }
 
+
+int countwords(char myphr[100])
+{
+	bool inword = false;
+	int	words = 0;
+	int i;
+	for (i = 0; i < strlen(myphr); i++)
+	{
+		if (myphr[i] !=' ' && !inword)
+		{
+			words++;
+			inword = true;
+		}
+		else if (myphr[i] == ' ')
+		{
+		inword = false;
+		}
+	}
+	return words;
+}
